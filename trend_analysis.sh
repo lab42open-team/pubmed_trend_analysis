d4f929aef13f1923c080778d51be080a4274d31b
s.paragkamian <s.paragkamian@hcmr.gr>
2021-04-08 14:01:32 +0300

    impove user input parameters

--- trend_analysis.sh
+++ trend_analysis.sh
@@ -1,34 +1,55 @@
 #!/bin/bash
 # this script is about finding matches of keywords in the pubmed tsv files
-
+#
+# Author:   Savvas Paragkamian (s.paragkamian@hcmr.gr)
+#           Institute of Marine Biology Biotechnology and Aquaculture (IMBBC)
+#           Hellenic Centre for Marine Research (HCMR)
+#
+# Created:  05/06/2020
+# License:  GNU GPLv3 license
+# Developer : Savvas Paragkamian
+#
+#
 ########################### user arguments#####################################
 # There are 3 user arguments. -k is for the keywords, -d is for pubmed destination and -p is for the user defined prefix of all files created from the analysis (e.g text files and plots). -d has a "default" option to use the inhouse pubmed downloads.
 
-while [ "$1" != "" ]; do
-  case $1 in
-    -k | --keywords )
-      shift # after the match of the -k | --keywords it moves the counter to the next number to take the import. Otherwise the value of $1 would have been "-k" and not the user argument
-      user_keywords=$1
-      ;;
+## Usage of the script
+usage="Use the parameter -k for the keywords file, -d for the corpus directory (expects the path to the PubMed data) and -p a string with prefix of all the generated files (txt and plots). \nExample: ./trend_analysis.sh -k keywords.txt -d default -p "ecology_trends" \n"
 
-    -d | --data )
-      shift
-      if [ "$1" == "default" ];then # the default option of Pubmed location
-        pubmed_path="/data/databases/pubmed/"
-      else
-        pubmed_path=$1
-      fi
-      ;;
-    -p | --prefix)
-      shift
-      user_prefix=$1
-      ;;
-
-  esac
-  shift
-
+## User input parameters of keywords, corpus directore and prefix of the analysis.
+while getopts "k:d:p:" option
+do
+   case "$option" in
+        k)   user_keywords="${OPTARG}";;
+        d)   pubmed_path="${OPTARG}"
+            if [ "$pubmed_path" == "default" ];then # the default option of Pubmed location
+                pubmed_path="/data/databases/pubmed/"
+            else
+                pubmed_path=$1
+            fi;;
+        p) user_prefix="${OPTARG}";;
+        ?|:)   echo -e "$usage" ; exit 1;;
+        *)   echo -e "option ${OPTARG} unknown. \n$usage" ; exit 1 ;;
+   esac
 done
 
+## Detect if no options were passed
+if [ $OPTIND -eq 1 ]; 
+    then echo -e "No options were passed. \n$usage "; exit 1; 
+fi
+
+## Detect if a single option is missing
+if [ -z "${user_keywords}" ]; then
+    echo -e "Option -k empty. $usage"; exit 1;
+fi
+
+if [ -z "${pubmed_path}" ]; then
+    echo -e "Option -d empty. $usage"; exit 1;
+fi
+if [ -z "${user_prefix}" ]; then
+    echo -e "Option -p empty. $usage"; exit 1;
+fi
+# Initiation of Trend Analysis
 echo 'The arguments passed are: '
 echo 'keywords file: ' $user_keywords
 echo 'PubMed folder: '$pubmed_path
